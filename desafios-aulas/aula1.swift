1dd870b2a659f7138bfc288d9777a0d067c21f65
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2024-05-06 13:44:11 -0300

    Update aula1.swift

--- desafios-aulas/aula1.swift
+++ desafios-aulas/aula1.swift
@@ -4,30 +4,6 @@ struct Desafio1: View {
     var body: some View {
         VStack {
             HStack{
-                Image("flor")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 150, height: 150)
-                    .cornerRadius(100)
-                Spacer()
-                VStack(spacing: 10) {
-                    Text("Hackatruck")
-                        .foregroundColor(.red)
-                    Text("77 universidades")
-                        .foregroundColor(.blue)
-                    Text("5 regiões no Brasil")
-                        .foregroundColor(.yellow)
-                }
-            }
-        }
-        .padding()
-    }
-}
-
-struct Desafio2: View {
-    var body: some View {
-        VStack {
-            HStack{
                 Rectangle()
                     .frame(width: 100, height: 100)
                     .foregroundColor(.red)
@@ -45,6 +21,30 @@ struct Desafio2: View {
                 Rectangle()
                     .frame(width: 100, height: 100)
                     .foregroundColor(.yellow)
+            }
+        }
+        .padding()
+    }
+}
+
+struct Desafio2: View {
+    var body: some View {
+        VStack {
+            HStack{
+                Image("flor")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(100)
+                Spacer()
+                VStack(spacing: 10) {
+                    Text("Hackatruck")
+                        .foregroundColor(.red)
+                    Text("77 universidades")
+                        .foregroundColor(.blue)
+                    Text("5 regiões no Brasil")
+                        .foregroundColor(.yellow)
+                }
             }
         }
         .padding()
