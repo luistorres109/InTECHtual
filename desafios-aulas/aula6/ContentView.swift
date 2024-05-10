d1c633e5f5d2f65cbcf712176091ddcfa7a5553b
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2024-05-10 13:59:59 -0300

    Create ContentView.swift

---
+++ desafios-aulas/aula6/ContentView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    @StateObject var x = ViewModel()
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(height: 1000)
+            
+            ScrollView(){
+                ForEach(x.chars){ c in
+                    HStack{
+                        AsyncImage(url: URL(string: c.image!)) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 90, height: 90)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        
+                        VStack{
+                            Text(c.name!)
+                                .frame(width: 275, alignment: .leading)
+                            Text(c.house!)
+                                .frame(width: 275, alignment: .leading)
+                                .bold()
+                        }
+                    }
+                    
+                    
+                    Spacer()
+                        .frame(height: 25)
+                    VStack{
+                        Spacer()
+                    }
+                }
+            }
+        }
+        .onAppear(){
+            x.fetch()
+        }
+    }
+}
