7c09783bfb21ebd2610b5907ae219cbc2bf68f0d
Luís Miguel Torres <94331066+luistorres109@users.noreply.github.com>
2024-05-10 15:52:15 -0300

    Update SheetView.swift

--- desafios-aulas/aula5/SheetView.swift
+++ desafios-aulas/aula5/SheetView.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import MapKit
 
 struct SheetView: View {
     @State  var x: Location
@@ -21,8 +20,4 @@ struct SheetView: View {
             .frame(height: 150)
         Text(x.description)
     }
-}
-
-#Preview {
-    SheetView(x: Location(name:"Belo Horizonte", coordinate: CLLocationCoordinate2D(latitude: -19.924557, longitude: -43.991597), flag:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9odmN0oTnXho1ldt-pNAXnqid1cBnq2tontGAcPxObw&s", description: "Belo Horizonte é a capital do estado de Minas Gerais, no sudeste do Brasil.", pais: "Brasil"))
 }
